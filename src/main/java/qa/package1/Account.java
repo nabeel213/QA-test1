a84a40abdf624aaf4f43432df4197e92552ff03a
nabeel <nabeel.94@live.com>
2018-04-18 16:23:10 +0100

    Second commit

--- src/main/java/qa/package1/Account.java
+++ src/main/java/qa/package1/Account.java
@@ -4,24 +4,42 @@ public class Account {
 
 	public static void main(String[] args) {
 		
+		
+		Account person1 = new Account("Marcell", "Illes", 001);
+		
+		Service s = new Service();
+		s.addCustomer(person1);
+	
 	}
+	
 	private String customerName;
 	private String customerLastName;
 	private int customerAccountNumber;
 	private int customerID;
 	
-	public Account(int id, String firstName, String lastName, int accountNumber) {
+	public Account(String firstName, String lastName, int accountNumber) {
 		
-		this.customerID = id;
+		//this.customerID = id;
 		this.customerName = firstName;
 		this.customerLastName = lastName;
 		this.customerAccountNumber = accountNumber;
 		
 	}
-
-	
 }
 
+/*
+Map<String,Object> map = new HashMap<>();
+//You can convert any Object.
+String[] value1 = new String[] { "value11", "value12", "value13" };
+String[] value2 = new String[] { "value21", "value22", "value23" };
+map.put("key1", value1);
+map.put("key2", value2);
+map.put("key3","string1");
+map.put("key4","string2");
+
+String json = new ObjectMapper().writeValueAsString(map);
+System.out.println(json);
+*/
 
 
 /*
--- src/main/java/qa/package1/Service.java
+++ src/main/java/qa/package1/Service.java
@@ -1,5 +1,6 @@
 package qa.package1;
 
+import java.util.*;
 import java.util.HashMap;
 
 public class Service {
@@ -12,6 +13,14 @@ public class Service {
 	void addCustomer(Account newAccount) {
 		
 		hashMap.put(uniqueID, newAccount);
+		
+		//System.out.println(hashMap.size());
+		
+		for()
+		
+		
+		
+		
 		uniqueID ++;
 	}
 	
@@ -27,5 +36,16 @@ public class Service {
 	 }
 }
 
-chad 
--r 
+/*
+Map<String,Object> map = new HashMap<>();
+//You can convert any Object.
+String[] value1 = new String[] { "value11", "value12", "value13" };
+String[] value2 = new String[] { "value21", "value22", "value23" };
+map.put("key1", value1);
+map.put("key2", value2);
+map.put("key3","string1");
+map.put("key4","string2");
+
+String json = new ObjectMapper().writeValueAsString(map);
+System.out.println(json);
+*/
